898b871e698fa1e9273185633c7bc4ebbf63223f
Tim K <tim750233@gmail.com>
2015-02-19 00:21:13 +0000

    form ready

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -3,6 +3,37 @@ class ProposalsController < ApplicationController
   end
 
   def new
-    @proposal = Proposal.new
+    session[:proposal_params] ||= {}
+    @proposal = Proposal.new(session[:proposal_params])
+    @proposal.current_step = session[:proposal_step]
+  end
+
+  def create
+    session[:proposal_params].deep_merge!(params[:proposal]) if params[:proposal]
+    @proposal = Proposal.new(session[:proposal_params])
+    @proposal.current_step = session[:proposal_step]
+    if @proposal.valid?
+      if params[:back_button]
+        @proposal.previous_step
+      elsif @proposal.final_step?
+        @proposal.save if @proposal.all_valid?
+      else
+        @proposal.next_step
+      end
+      session[:proposal_step] = @proposal.current_step
+    end
+    if @proposal.new_record?
+      render 'new'
+    else
+      flash[:success] = 'Temporary notice (report generated!)'
+      session[:proposal_step] = session[:proposal_params] = nil
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def get_params
+    session.require(:proposal).permit!
   end
 end
